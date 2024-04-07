4187798e6d7124413429d059e87c4b4f49a8249d
Marjon Cajocon <88709877+marjoncajocon@users.noreply.github.com>
2024-04-07 10:55:36 +0800

    Add table support

--- control.js
+++ control.js
@@ -2,6 +2,7 @@ var ControlConfig = {
     Colors: ['primary', 'danger', 'accent'],
     ButtonType: ['flat', 'raised', 'fab'],
     TextFieldType: ['float'],
+    TableType: ['bordered'],
     GridType: ['md', 'xs', 'lg', 'sm', 'xl', 'md-offset', 'xs-offset', 'lg-offset', 'sm-offset', 'xl-offset'], //        var reg = new RegExp('^[md|xs|lg|sm|xl]\-$');
     tostr: function(ls) {
         var ret = '';
@@ -21,9 +22,44 @@ var __isValidConfig = function(n, ls) {
 };
 
 
-var Panel = function() {
+var Container = function() {
     var panel = new JPanel().addClass('mui-container-fluid');
 
+
+    this.setStyle = function(obj_dic) {
+        panel.setStyle(obj_dic);
+    };
+
+    this.addClass = function(classname) {
+        panel.addClass(classname);
+    };
+
+    this.control = function() {
+        return panel;
+    };
+
+    this.add = function(j) {
+        if (j instanceof JInterface) {
+            panel.add(j);
+        } else {
+            panel.add(j.control());
+        }
+    };
+};
+
+var Panel = function() {
+    var panel = new JPanel();
+
+
+    var self = this;
+    this.setStyle = function(obj_dic) {
+        panel.setStyle(obj_dic);
+        return self;
+    };
+
+    this.addClass = function(classname) {
+        panel.addClass(classname);
+    };
 
     this.control = function() {
         return panel;
@@ -62,6 +98,10 @@ var Button = function(label, color, type) {
     if (type != '') {
         b.addClass('mui-btn--' + type);
     }
+
+    this.addEvent = function(e, c) {
+        b.addEvent(e, c);
+    };
 
     this.control = function() {
         return b;
@@ -161,3 +201,78 @@ var Grid = function() {
         return panel;
     };
 };
+
+
+/* Table */
+
+var Table = function(header, type) {
+    var table = new JTable().addClass('mui-table');
+    var type = type || '';
+
+    if (!(header instanceof Array)) {
+        throw new TypeError('Table Contructor header must a Array or List, example: ["no", "name", "age"..etc]');
+    }
+
+    if (type != '') {
+        if (!__isValidConfig(type, ControlConfig.TableType)) throw new TypeError(type + ' is not valid: valid: ' + ControlConfig.tostr(ControlConfig.TableType));
+
+        table.addClass('mui-table--' + type);
+    }
+
+    var thead = new JThead();
+
+    var tr = new JTr();
+
+    for (var i in header) {
+        var th = new JTh();
+        
+        if (header[i] instanceof JInterface) {
+            th.add(header[i]);
+        } else if (header[i] instanceof Object) {
+            th.add(header[i].control());
+        } else {
+            th.setText(header[i]);
+        }
+
+        tr.add(th);
+    }
+    thead.add(tr);
+    table.add(thead);
+
+    var tbody = new JTbody();
+    table.add(tbody);
+
+    this.clear = function() {
+        tbody.clear();
+    };
+
+    this.addRow = function(arr) {
+        if (!(arr instanceof Array)) throw new TypeError('Table.addRow must be an List or an array sample: [1, "argon", 23]');
+        if (header.length != arr.length) throw new Error('Table header must match the size of the addRow or table data');
+
+        var tr = new JTr();
+        for (var i in arr) {
+            var td = new JTd();
+
+            if (arr[i] instanceof JInterface) {
+                td.add(arr[i]);
+            } else if (arr[i] instanceof Object) {
+                td.add(arr[i].control());
+            } else {
+                td.setText(arr[i]);
+            }
+
+            tr.add(td);
+        }
+        tbody.add(tr);
+    };
+
+    this.control = function() {
+        return table;
+    }
+};
+
+/*
+var table = Table();
+
+*/
