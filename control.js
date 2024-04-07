eff2f74fe0c931c488d86eafbf72559d3b3fb4e2
Marjon Cajocon <88709877+marjoncajocon@users.noreply.github.com>
2024-04-07 09:55:26 +0800

    Add type checking error and dynamic add of JInterface and extended controls control.js

--- control.js
+++ control.js
@@ -1,7 +1,58 @@
+var ControlConfig = {
+    Colors: ['primary', 'danger', 'accent'],
+    ButtonType: ['flat', 'raised', 'fab'],
+    TextFieldType: ['float'],
+    GridType: ['md', 'xs', 'lg', 'sm', 'xl', 'md-offset', 'xs-offset', 'lg-offset', 'sm-offset', 'xl-offset'], //        var reg = new RegExp('^[md|xs|lg|sm|xl]\-$');
+    tostr: function(ls) {
+        var ret = '';
+        for (var i in ls) {
+            ret += ls[i] + ','
+        }
+        return ret;
+    }
+};
+var __isValidConfig = function(n, ls) {
+    for (var i in ls) {
+        if (n == ls[i]) {
+            return true;
+        }
+    }
+    return false;
+};
+
+
+var Panel = function() {
+    var panel = new JPanel().addClass('mui-container-fluid');
+
+
+    this.control = function() {
+        return panel;
+    };
+
+    this.add = function(j) {
+        if (j instanceof JInterface) {
+            panel.add(j);
+        } else {
+            panel.add(j.control());
+        }
+    };
+};
+
+
 var Button = function(label, color, type) {
+    
     var label = label || ''; /* Label of the button */
     var color = color || ''; /* primary, danger, accent */
     var type = type || ''; /* flat, raised, fab */ /* fab is a circular button */
+
+    /* checking error  */
+    if (color != '') {
+        if (!__isValidConfig(color, ControlConfig.Colors)) throw new TypeError(color + ' is not valid! valid input: ' + ControlConfig.tostr(ControlConfig.Colors));
+    }
+    if (type != '') {
+        if (!__isValidConfig(type, ControlConfig.ButtonType)) throw new TypeError(type + ' is not valid! valid input: ' + ControlConfig.tostr(ControlConfig.ButtonType));
+    }
+    /* End error checking */
 
     var b = new JButton(label).addClass('mui-btn');
     if (color != '') {
@@ -18,14 +69,18 @@ var Button = function(label, color, type) {
 };
 
 
-var TextField = function(label, is_float) {
+var TextField = function(label, _float) {
     var label = label || '';
-    var is_float = is_float || ''; /* float or if not empty then auto matic to float */
+    var _float = _float || '';
+
+    if (_float != '') {
+        if (!__isValidConfig(_float, ControlConfig.TextFieldType)) throw new TypeError(type + ' is not valid! valid: ' + ControlConfig.TextFieldType);
+    }
 
     var panel = new JPanel();
     panel.addClass('mui-textfield');
 
-    if (is_float != '') {
+    if (_float != '') {
         panel.addClass('mui-textfield--float-label');
     }
 
@@ -52,13 +107,53 @@ var Grid = function() {
 
 
     this.cell = function(container, size) {
+        
+        /* Checking Errors  */
+        if ( !(size instanceof Array) ) {
+            throw new TypeError('function(container, size) {} the value of size must be a List or Array([]), example: md-5, md-3');
+        }
+
+        for (var index in size) {
+            var n = size[index];
+            var a = n.split('-');
+            
+            var t = '';
+            if (a.length > 2) {
+                n = a[0] + '-' + a[1] + '-' + a[2];
+                t = a[0] + '-' + a[1];
+
+                if (!__isValidConfig(t, ControlConfig.GridType)) throw new Error(t + ' is invalid: valid: ' + ControlConfig.tostr(ControlConfig.GridType));
+
+                if (parseInt(a[2]) > 12) {
+                    throw new Error('the Grid, col maximum of 12 only!');
+                }
+            } else {
+                t = a[0];
+                
+                if (!__isValidConfig(t, ControlConfig.GridType)) throw new Error(t + ' is invalid: valid: ' + ControlConfig.tostr(ControlConfig.GridType));
+
+                if (parseInt(a[1]) > 12) {
+                    throw new Error('the Grid, col maximum of 12 only');
+                }
+            }
+
+        };
+
+        /* End Checking errors */
+
         /*  size must be a list or array of string ex.: md-1 */
-        var size = size || []; /* md-no, sm-no, xs-no, lg-no  */
+        var size = size || []; /* md-no, sm-no, xs-no, lg-no  */ /* also : mui-col-md-offset-4  */
         var spanel = new JPanel();
         for (var x in size) {
             spanel.addClass('mui-col-' + size[x]);
+            /* mui-col-md-offset-4  */
         }
-        spanel.add(container);
+        if (container instanceof JInterface) {
+            spanel.add(container);
+        } else {
+            spanel.add(container.control());
+        }
+        
         panel.add(spanel);
     };
 
