a06e673305efe45b42a69f9fc91c3b4ea527f66b
Marjon Cajocon <88709877+marjoncajocon@users.noreply.github.com>
2024-08-23 23:15:53 +0800

    Create controlv2.js
    initial version 2 controljs,,,

---
+++ controlv2.js
@@ -0,0 +1,145 @@
+class Widget {
+
+  constructor(h) {
+    this.control = document.createElement(h);
+    this.body = document.body;
+
+  }
+
+  clear() {
+    
+    while (this.control.firstChild) {
+      
+      this.control.firstChild.remove();
+      
+    }
+
+    return this;
+  }
+
+  setStyle(styles = {}) {
+
+    for (const item in styles) {
+      this.control.style[item] = styles[item];
+    }
+
+    return this;
+  }
+
+  show() {
+    this.control.style.display = 'inline-block';
+    return this;
+  }
+
+  hide() {
+    this.control.style.display = 'none';
+    return this;
+  }
+
+  add(widget) {
+    this.control.appendChild(widget.control);
+    return this;
+  }
+
+  addEventListener(evt, fn) {
+    this.control.addEventListener(evt, fn);
+
+    return this;
+  }
+}
+
+// Text 
+class Text extends Widget {
+  constructor(param = '') {
+
+    super('label');
+    if (typeof(param) == 'string') {
+      this.control.innerHTML = param;
+    }
+
+    if (typeof(param) != 'object') return;
+
+
+  }
+}
+
+// Button
+
+class Button extends Widget {
+  constructor(param) {
+    super('button', param);
+
+    if (typeof(param) == 'string') {
+      this.add(new Text(param));
+    }
+    
+
+    
+    if (typeof(param) != 'object') return;
+    
+    const { title = '', widgets = [] } = param; // default param
+
+    this.add(new Text(title));
+
+    for (const widget of widgets) {
+      this.add(widget);
+    }
+
+  }
+}
+
+// TextField 
+
+class TextField extends Widget {
+  constructor() {
+    super('input');
+
+  }
+}
+
+// TextBox
+
+class TextBox extends Widget {
+  constructor() {
+    super('textarea');
+
+  }
+}
+
+// Fieldset
+
+class Legend extends Widget {
+  constructor(param = '') {
+    super('legend');
+    if (typeof(param) == 'string') {
+      this.add(new Text(param));
+    }
+
+  }
+}
+
+class FieldSet extends Widget {
+  constructor(param = 'str') {
+    super('fieldset');
+    if (typeof(param) == 'string') {
+      this.add(new Legend(param));
+    }
+
+  }
+}
+
+
+
+
+// Window
+class Window extends Widget {
+  constructor({ app = null, title = '', width = null, height = null }) {
+    super('div');
+
+    this.hide();
+    this.body.appendChild(this.control);
+  }
+  run() {
+    this.show();
+  }
+}
