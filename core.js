3d4ca376d5679c34018a8829b59d0a3e34be85e0
Marjon Cajocon <88709877+marjoncajocon@users.noreply.github.com>
2024-12-04 09:27:06 +0800

    Update core.js

--- core.js
+++ core.js
@@ -106,6 +106,14 @@ class Widget {
 
   }
 
+  html() {
+
+  }
+
+  text() {
+
+  }
+
   show() {
     this.control.style.display = 'inline-block';
     return this;
@@ -156,125 +164,159 @@ class Widget {
 
 }
 
-class Html extends Widget { constructor() { super("html"); } }
-class Head extends Widget { constructor() { super("head"); } }
-class Body extends Widget { constructor() { super("body"); } }
-class Title extends Widget { constructor() { super("title"); } }
-class Base extends Widget { constructor() { super("base"); } }
-class Link extends Widget { constructor() { super("link"); } }
-class Meta extends Widget { constructor() { super("meta"); } }
-class Style extends Widget { constructor() { super("style"); } }
-class Script extends Widget { constructor() { super("script"); } }
-class Noscript extends Widget { constructor() { super("noscript"); } }
-class Template extends Widget { constructor() { super("template"); } }
+class html extends Widget { constructor() { super("html"); } }
+class head extends Widget { constructor() { super("head"); } }
+class body extends Widget { constructor() { super("body"); } }
+class title extends Widget { constructor() { super("title"); } }
+class base extends Widget { constructor() { super("base"); } }
+class link extends Widget { constructor() { super("link"); } }
+class meta extends Widget { constructor() { super("meta"); } }
+class style extends Widget { constructor() { super("style"); } }
+class script extends Widget { constructor() { super("script"); } }
+class noscript extends Widget { constructor() { super("noscript"); } }
+class template extends Widget { constructor() { super("template"); } }
 
-class Section extends Widget { constructor() { super("section"); } }
-class Nav extends Widget { constructor() { super("nav"); } }
-class Article extends Widget { constructor() { super("article"); } }
-class Aside extends Widget { constructor() { super("aside"); } }
-class H1 extends Widget { constructor() { super("h1"); } }
-class H2 extends Widget { constructor() { super("h2"); } }
-class H3 extends Widget { constructor() { super("h3"); } }
-class H4 extends Widget { constructor() { super("h4"); } }
-class H5 extends Widget { constructor() { super("h5"); } }
-class H6 extends Widget { constructor() { super("h6"); } }
-class Header extends Widget { constructor() { super("header"); } }
-class Footer extends Widget { constructor() { super("footer"); } }
+class section extends Widget { constructor() { super("section"); } }
+class nav extends Widget { constructor() { super("nav"); } }
+class article extends Widget { constructor() { super("article"); } }
+class aside extends Widget { constructor() { super("aside"); } }
+class h1 extends Widget { constructor() { super("h1"); } }
+class h2 extends Widget { constructor() { super("h2"); } }
+class h3 extends Widget { constructor() { super("h3"); } }
+class h4 extends Widget { constructor() { super("h4"); } }
+class h5 extends Widget { constructor() { super("h5"); } }
+class h6 extends Widget { constructor() { super("h6"); } }
+class header extends Widget { constructor() { super("header"); } }
+class footer extends Widget { constructor() { super("footer"); } }
 
-class Address extends Widget { constructor() { super("address"); } }
-class P extends Widget { constructor() { super("p"); } }
-class Hr extends Widget { constructor() { super("hr"); } }
-class Pre extends Widget { constructor() { super("pre"); } }
-class Blockquote extends Widget { constructor() { super("blockquote"); } }
-class Ol extends Widget { constructor() { super("ol"); } }
-class Ul extends Widget { constructor() { super("ul"); } }
-class Li extends Widget { constructor() { super("li"); } }
-class Dl extends Widget { constructor() { super("dl"); } }
-class Dt extends Widget { constructor() { super("dt"); } }
-class Dd extends Widget { constructor() { super("dd"); } }
-class Figure extends Widget { constructor() { super("figure"); } }
-class Figcaption extends Widget { constructor() { super("figcaption"); } }
-class Main extends Widget { constructor() { super("main"); } }
-class Div extends Widget { constructor() { super("div"); } }
+class address extends Widget { constructor() { super("address"); } }
+class p extends Widget { constructor() { super("p"); } }
+class hr extends Widget { constructor() { super("hr"); } }
+class pre extends Widget { constructor() { super("pre"); } }
+class blockquote extends Widget { constructor() { super("blockquote"); } }
+class ol extends Widget { constructor() { super("ol"); } }
+class ul extends Widget { constructor() { super("ul"); } }
+class li extends Widget { constructor() { super("li"); } }
+class dl extends Widget { constructor() { super("dl"); } }
+class dt extends Widget { constructor() { super("dt"); } }
+class dd extends Widget { constructor() { super("dd"); } }
+class figure extends Widget { constructor() { super("figure"); } }
+class figcaption extends Widget { constructor() { super("figcaption"); } }
+class main extends Widget { constructor() { super("main"); } }
+class div extends Widget { constructor() { super("div"); } }
 
-class A extends Widget { constructor() { super("a"); } }
-class Em extends Widget { constructor() { super("em"); } }
-class Strong extends Widget { constructor() { super("strong"); } }
-class Small extends Widget { constructor() { super("small"); } }
-class S extends Widget { constructor() { super("s"); } }
-class Cite extends Widget { constructor() { super("cite"); } }
-class Q extends Widget { constructor() { super("q"); } }
-class Dfn extends Widget { constructor() { super("dfn"); } }
-class Abbr extends Widget { constructor() { super("abbr"); } }
-class Ruby extends Widget { constructor() { super("ruby"); } }
-class Rt extends Widget { constructor() { super("rt"); } }
-class Rp extends Widget { constructor() { super("rp"); } }
-class B extends Widget { constructor() { super("b"); } }
-class I extends Widget { constructor() { super("i"); } }
-class U extends Widget { constructor() { super("u"); } }
-class Mark extends Widget { constructor() { super("mark"); } }
-class Bdi extends Widget { constructor() { super("bdi"); } }
-class Bdo extends Widget { constructor() { super("bdo"); } }
-class Span extends Widget { constructor() { super("span"); } }
-class Br extends Widget { constructor() { super("br"); } }
-class Wbr extends Widget { constructor() { super("wbr"); } }
+class a extends Widget { constructor() { super("a"); } }
+class em extends Widget { constructor() { super("em"); } }
+class strong extends Widget { constructor() { super("strong"); } }
+class small extends Widget { constructor() { super("small"); } }
+class s extends Widget { constructor() { super("s"); } }
+class cite extends Widget { constructor() { super("cite"); } }
+class q extends Widget { constructor() { super("q"); } }
+class dfn extends Widget { constructor() { super("dfn"); } }
+class abbr extends Widget { constructor() { super("abbr"); } }
+class ruby extends Widget { constructor() { super("ruby"); } }
+class rt extends Widget { constructor() { super("rt"); } }
+class rp extends Widget { constructor() { super("rp"); } }
+class b extends Widget { constructor() { super("b"); } }
+class i extends Widget { constructor() { super("i"); } }
+class u extends Widget { constructor() { super("u"); } }
+class mark extends Widget { constructor() { super("mark"); } }
+class bdi extends Widget { constructor() { super("bdi"); } }
+class bdo extends Widget { constructor() { super("bdo"); } }
+class span extends Widget { constructor() { super("span"); } }
+class br extends Widget { constructor() { super("br"); } }
+class wbr extends Widget { constructor() { super("wbr"); } }
 
-class Ins extends Widget { constructor() { super("ins"); } }
-class Del extends Widget { constructor() { super("del"); } }
+class ins extends Widget { constructor() { super("ins"); } }
+class del extends Widget { constructor() { super("del"); } }
 
-class Img extends Widget { constructor() { super("img"); } }
-class Iframe extends Widget { constructor() { super("iframe"); } }
-class Embed extends Widget { constructor() { super("embed"); } }
-class Object extends Widget { constructor() { super("object"); } }
-class Param extends Widget { constructor() { super("param"); } }
-class Video extends Widget { constructor() { super("video"); } }
-class Audio extends Widget { constructor() { super("audio"); } }
-class Source extends Widget { constructor() { super("source"); } }
-class Track extends Widget { constructor() { super("track"); } }
-class Canvas extends Widget { constructor() { super("canvas"); } }
-class Map extends Widget { constructor() { super("map"); } }
-class Area extends Widget { constructor() { super("area"); } }
-class Svg extends Widget { constructor() { super("svg"); } }
-class Math extends Widget { constructor() { super("math"); } }
+class img extends Widget { constructor() { super("img"); } }
+class iframe extends Widget { constructor() { super("iframe"); } }
+class embed extends Widget { constructor() { super("embed"); } }
+class object extends Widget { constructor() { super("object"); } }
+class param extends Widget { constructor() { super("param"); } }
+class video extends Widget { constructor() { super("video"); } }
+class audio extends Widget { constructor() { super("audio"); } }
+class source extends Widget { constructor() { super("source"); } }
+class track extends Widget { constructor() { super("track"); } }
+class canvas extends Widget { constructor() { super("canvas"); } }
+class map extends Widget { constructor() { super("map"); } }
+class area extends Widget { constructor() { super("area"); } }
+class svg extends Widget { constructor() { super("svg"); } }
+class math extends Widget { constructor() { super("math"); } }
 
-class Table extends Widget { constructor() { super("table"); } }
-class Caption extends Widget { constructor() { super("caption"); } }
-class Colgroup extends Widget { constructor() { super("colgroup"); } }
-class Col extends Widget { constructor() { super("col"); } }
-class Tbody extends Widget { constructor() { super("tbody"); } }
-class Thead extends Widget { constructor() { super("thead"); } }
-class Tfoot extends Widget { constructor() { super("tfoot"); } }
-class Tr extends Widget { constructor() { super("tr"); } }
-class Td extends Widget { constructor() { super("td"); } }
-class Th extends Widget { constructor() { super("th"); } }
+class table extends Widget { constructor() { super("table"); } }
+class caption extends Widget { constructor() { super("caption"); } }
+class colgroup extends Widget { constructor() { super("colgroup"); } }
+class col extends Widget { constructor() { super("col"); } }
+class tbody extends Widget { constructor() { super("tbody"); } }
+class thead extends Widget { constructor() { super("thead"); } }
+class tfoot extends Widget { constructor() { super("tfoot"); } }
+class tr extends Widget { constructor() { super("tr"); } }
+class td extends Widget { constructor() { super("td"); } }
+class th extends Widget { constructor() { super("th"); } }
 
-class Form extends Widget { constructor() { super("form"); } }
-class Label extends Widget { constructor() { super("label"); } }
-class Input extends Widget { constructor() { super("input"); } }
-class Button extends Widget { constructor() { super("button"); } }
-class Select extends Widget { constructor() { super("select"); } }
-class Datalist extends Widget { constructor() { super("datalist"); } }
-class Optgroup extends Widget { constructor() { super("optgroup"); } }
-class Option extends Widget { constructor() { super("option"); } }
-class Textarea extends Widget { constructor() { super("textarea"); } }
-class Fieldset extends Widget { constructor() { super("fieldset"); } }
-class Legend extends Widget { constructor() { super("legend"); } }
-class Progress extends Widget { constructor() { super("progress"); } }
-class Meter extends Widget { constructor() { super("meter"); } }
-class Output extends Widget { constructor() { super("output"); } }
+class form extends Widget { constructor() { super("form"); } }
+class label extends Widget { constructor() { super("label"); } }
+class input extends Widget { constructor() { super("input"); } }
+class button extends Widget { constructor() { super("button"); } }
+class select extends Widget { constructor() { super("select"); } }
+class datalist extends Widget { constructor() { super("datalist"); } }
+class optgroup extends Widget { constructor() { super("optgroup"); } }
+class option extends Widget { constructor() { super("option"); } }
+class textarea extends Widget { constructor() { super("textarea"); } }
+class fieldset extends Widget { constructor() { super("fieldset"); } }
+class legend extends Widget { constructor() { super("legend"); } }
+class progress extends Widget { constructor() { super("progress"); } }
+class meter extends Widget { constructor() { super("meter"); } }
+class output extends Widget { constructor() { super("output"); } }
 
-class Details extends Widget { constructor() { super("details"); } }
-class Summary extends Widget { constructor() { super("summary"); } }
-class Dialog extends Widget { constructor() { super("dialog"); } }
+class details extends Widget { constructor() { super("details"); } }
+class summary extends Widget { constructor() { super("summary"); } }
+class dialog extends Widget { constructor() { super("dialog"); } }
 
+
+
+class Window extends Widget {
+  constructor(param) {
+    super('div');
+    this.setStyle({
+      width: '100%',
+      height: '100%',
+      position: 'relative'
+    });
+    if (typeof(param) == 'object') {
+      const { app = null, title = '', width = null, height = null } = param;
+    }
+    this.hide();
+    this.body.appendChild(this.control);
+  }
+
+  navigate(obj) {
+    this.clear();
+    this.add(obj);
+  } 
+
+  run() {
+    this.show();
+  }
+}
+
+
+// core
 export {
-  Html, Head, Body, Title, Base, Link, Meta, Style, Script, Noscript, Template,
-  Section, Nav, Article, Aside, H1, H2, H3, H4, H5, H6, Header, Footer, Address,
-  P, Hr, Pre, Blockquote, Ol, Ul, Li, Dl, Dt, Dd, Figure, Figcaption, Main, Div,
-  A, Em, Strong, Small, S, Cite, Q, Dfn, Abbr, Ruby, Rt, Rp, B, I, U, Mark, Bdi,
-  Bdo, Span, Br, Wbr, Ins, Del, Img, Iframe, Embed, Object, Param, Video, Audio,
-  Source, Track, Canvas, Map, Area, Svg, Math, Table, Caption, Colgroup, Col,
-  Tbody, Thead, Tfoot, Tr, Td, Th, Form, Label, Input, Button, Select, Datalist,
-  Optgroup, Option, Textarea, Fieldset, Legend, Progress, Meter, Output, Details,
-  Summary, Dialog
+  html, head, body, title, base, Link, meta, style, script, noscript, template,
+  section, nav, article, aside, h1, h2, h3, h4, h5, h6, header, footer, address,
+  p, hr, pre, blockquote, ol, ul, li, dl, dt, dd, figure, figcaption, main, div,
+  a, em, strong, small, s, cite, q, dfn, abbr, ruby, rt, rp, b, i, u, mark, bdi,
+  bdo, span, br, wbr, ins, del, img, iframe, embed, object, param, video, audio,
+  source, track, canvas, map, area, svg, math, table, caption, colgroup, col,
+  tbody, thead, tfoot, tr, td, th, form, label, input, button, select, datalist,
+  optgroup, option, textarea, fieldset, legend, progress, meter, output, details,
+  summary, dialog
+};
+
+// std 
+export {
+  Window
 };
